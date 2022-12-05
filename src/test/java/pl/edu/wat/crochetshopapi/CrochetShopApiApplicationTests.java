2ee1d968e29dde2ca5e4a126482dea9f9831405b
Klit3k <wojciech@klicki.xyz>
2022-12-05 10:13:01 +0100

    First commit

---
+++ src/test/java/pl/edu/wat/crochetshopapi/CrochetShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wat.crochetshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochetShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
